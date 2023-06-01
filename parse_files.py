2a620721da853c7730a6e578d4a1f7ecee546567
Will King <willking410@gmail.com>
2023-06-01 13:42:04 -0400

    added file_stats function

--- parse_files.py
+++ parse_files.py
@@ -26,10 +26,6 @@ def get_file_names(myType, myDf, mySource, myStart, myEnd):
             myDf.loc[len(myDf.index)] = [myType, item] 
     return myDf
 
-def get_symbols(myMaster):
-    myMaster['Symbol'] = myMaster.Filename.str.split(".").str[0]
-    return myMaster
-
 def build_master(myPath, myLookup, myStart, myEnd):
     myMaster = pd.DataFrame(columns=['Symbol', 'Type', 'Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
     myLookup = myLookup.reset_index()
@@ -53,11 +49,29 @@ def build_master(myPath, myLookup, myStart, myEnd):
     myMaster = myMaster.reset_index(drop=True)
     return myMaster
 
+def file_stats(myPath, myLookup, myStart, myEnd):
+    myMaster = pd.DataFrame(columns=['Symbol', 'Type', 'Size'])
+    myLookup = myLookup.reset_index(drop = True)
+    
+    for index, row in myLookup.iterrows():
+        print("Fetching (" + str(index) + "/" + str(len(myLookup.index) - 1) + ") " + row['Filename'] + " data")
+        myFilePath = myPath+"/"+row['Type']+"/"+row['Filename']
+        
+        myStats = os.stat(myFilePath)
+        
+        fileDf = pd.read_csv(myFilePath)
+        fileDf['Date'] = pd.to_datetime(fileDf['Date'])
+        
+        myMaster.loc[len(myMaster.index)] = [row['Symbol'], row['Type'], (myStats.st_size / (1024 * 1024))]
+        
+    myMaster = myMaster.reset_index(drop=True)
+    return myMaster
+
 # define variables
 source = "desktop"
 startLetter = "a"
-endLetter = "c"
-startDate = "2018-01-01"
+endLetter = "z"
+startDate = "2015-01-01"
 endDate = "2020-12-31"
 
 # build path and blank dfs
@@ -77,6 +91,13 @@ print("Merge stock and etf tables")
 lookupFile = pd.concat([stockFiles,etfFiles], axis="rows")
 lookupFile['Symbol'] = lookupFile.Filename.str.split(".").str[0]
 
+"""
+# get file size stats
+fileStats = file_stats(absolutePath, lookupFile, startDate, endDate)
+print(fileStats)
+fileStats.to_csv(absolutePath + "file_size_" + str(date.today()) + ".csv")
+
+"""
 # read through individual fields to extract data
 print("Build masterFile")
 masterFile = build_master(absolutePath, lookupFile, startDate, endDate)
